a047618e50aff2502e948a5940fc9ed0b3bbce1c
Hayden Le <haydenhle@yahoo.com>
2025-11-22 17:11:31 -0800

    Initial commit - base files for Sufia Exorcism

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Sufia Exorcism</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <?php include 'navbar.php'; ?>
+  <main>
+    <h1>Welcome to Sufia</h1>
+    <p>A post-apocalyptic fantasy world of demons and magicians.</p>
+    <a href="world.php" class="btn">Explore the World</a>
+  </main>
+</body>
+</html>
+
+<nav>
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="news.php">News</a></li>
+    <li><a href="world.php">World</a></li>
+    <li><a href="gameplay.php">Gameplay</a></li>
+    <li><a href="characters.php">Characters</a></li>
+    <li><a href="contact.php">Contact Us</a></li>
+  </ul>
+</nav>
